25e4c01728cd15c123ba2cef9c8e4ec6d5de3342
xiaochengdong <xiaochengdong0710@163.com>
2026-01-09 16:18:54 +0800

    fix: 根据说明文档进行适配

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,7 +16,7 @@ const getBasePath = () => {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteUrl: 'https://paulxiao.github.io/running_page/',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
@@ -24,10 +24,10 @@ const data: ISiteMetadataResult = {
       name: 'Summary',
       url: `${getBasePath()}/summary`,
     },
-    {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
-    },
+    // {
+    //   name: 'Blog',
+    //   url: 'https://paulxiao.github.io/running_page/',
+    // },
     {
       name: 'About',
       url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
